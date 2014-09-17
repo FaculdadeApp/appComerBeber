c5df4a89321c82de3323dafdc96f720775a31fce
Stharley Maxwell <stharleymaxwell@hotmail.com>
2014-09-16 23:34:13 -0300

    a

--- gen/br/com/comerbeberagora/R.java
+++ gen/br/com/comerbeberagora/R.java
@@ -1207,6 +1207,7 @@ containing a value of this type.
         public static final int main=0x7f030019;
         public static final int splash=0x7f03001a;
         public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int tab_categ=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0d0000;
